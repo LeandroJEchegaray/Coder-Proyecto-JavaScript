a8af21d19f7e9d90480da75be3bc7d55b72a4991
leandro echegaray <leandro.echegaray92@gmail.com>
2022-09-07 16:32:00 -0300

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,38 +5,59 @@ const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
 const listaProductos = document.querySelector("#lista-productos");
 const listaProductos2 = document.querySelector("#seccion-card");
 const total = document.querySelector(".cart-total");
-
+let cartTotal = 0;
 const confimarCompraBtn = document.querySelector("#confirmar-compra");
 
 carritoHTML()
 
-console.log(typeof(articulosCarrito))
+function calculateTotal() {
+  cartTotal = 0;
+  articulosCarrito.forEach((product) => {
+    cartTotal = (parseInt(product.precio) * product.cantidad) + cartTotal;
+  })
+  document.querySelector(".total-carrito").innerText = `$${cartTotal.toFixed(2)}`;
+}
 
-const productos = [
-  {
-      id:1,
-      title:"Titulo 1",
-      img: "assets/img/default.jpg",
-      price: 1000,
-      cant:0,
-      stock:10
+const productos = [{
+    id: 1,
+    title: "Lentes de sol Rusty vulk",
+    img: "assets/img/1.webp",
+    price: 2400,
+    cant: 0,
+    stock: 10
   },
   {
-      id:2,
-      title:"Titulo 2",
-      img: "assets/img/default.jpg",
-      price: 1100,
-      cant:0,
-      stock:10
+    id: 2,
+    title: "Lentes de sol Rusty polarizados orange",
+    img: "assets/img/2.webp",
+    price: 6600,
+    cant: 0,
+    stock: 10
 
   },
   {
-      id:3,
-      title:"Titulo 3",
-      img: "assets/img/default.jpg",
-      price: 1200,
-      cant:0,
-      stock:10
+    id: 3,
+    title: "Lentes de sol Rusty vulkan",
+    img: "assets/img/3.webp",
+    price: 4000,
+    cant: 0,
+    stock: 10
+  },
+  {
+    id: 4,
+    title: "Lentes de sol Rusty k-vulk 001",
+    img: "assets/img/4.webp",
+    price: 3460,
+    cant: 0,
+    stock: 10
+  },
+  {
+    id: 5,
+    title: "Lentes de sol Rusty Iron ",
+    img: "assets/img/5.webp",
+    price: 1230,
+    cant: 0,
+    stock: 10
   },
 ];
 //generar cards carrito
@@ -46,10 +67,12 @@ productos.forEach((producto) => {
       <div>
       <img class="img" src="${producto.img}" alt="">
       </div>
-      <span class="price-tag"><span>$</span>${producto.price}</span>
+      <div>
+      <span class="price-tag-symbol">$</span><span class="price-tag">${producto.price}</span>
+      </div>
       <span class="free-shipping">Envio gratis</span>
       <p class ="card-title">${producto.title}</p>
-      <div class="off30"">30% OFF</div>
+      
       <a href="#" class="btn agregar-carrito" id="${producto.id}">Comprar</a>
       </div>`;
 });
@@ -62,63 +85,61 @@ function cargarEventListeners() {
   listaProductos2.addEventListener("click", agregarProducto);
   carrito.addEventListener("click", eliminarProducto);
   vaciarCarritoBtn.addEventListener("click", () => {
-      
-    if (articulosCarrito.length==0) {
-        Swal.fire({
-            title: 'El carrito esta vacio',
-            text: 'Agrega algunos productos',
-            icon: 'error',
-            confirmButtonText: 'OK'
-        })
+
+    if (articulosCarrito.length == 0) {
+      Swal.fire({
+        title: 'El carrito esta vacio',
+        text: 'Agrega algunos productos',
+        icon: 'error',
+        confirmButtonText: 'OK'
+      })
     } else {
-        Swal.fire({
-            title: 'Carrito vaciado con éxito',
-            text: 'Agrega algunos productos',
-            icon: 'success',
-            confirmButtonText: 'OK'
-        })
-        articulosCarrito = [];
-        localStorage.setItem("articulosCarrito", JSON.stringify(articulosCarrito));
-        limpiarHTML();
+      Swal.fire({
+        title: 'Carrito vaciado con éxito',
+        text: 'Agrega algunos productos',
+        icon: 'success',
+        confirmButtonText: 'OK'
+      })
+      articulosCarrito = [];
+      localStorage.setItem("articulosCarrito", JSON.stringify(articulosCarrito));
+      limpiarHTML();
     }
-});
+  });
 
   confimarCompraBtn.addEventListener("click", () => {
-    if (articulosCarrito.length==0) {
-        Swal.fire({
-            title: 'El carrito esta vacio',
-            text: 'Agrega algunos productos',
-            icon: 'error',
-            confirmButtonText: 'OK',
-            confirmButtonColor: '#303030',
-        })
+    if (articulosCarrito.length == 0) {
+      Swal.fire({
+        title: 'El carrito esta vacio',
+        text: 'Agrega algunos productos',
+        icon: 'error',
+        confirmButtonText: 'OK',
+        confirmButtonColor: '#303030',
+      })
     } else {
 
-        Swal.fire({
-            title: '¿Confirmar compra?',
-            text: "Recuerda revisar bien tus datos",
-            icon: 'question',
-            showCancelButton: true,
-            confirmButtonColor: '#303030',
-            cancelButtonText:'Seguir comprando',
-            confirmButtonText: 'Confirmar'
-            }).then((result) => {
-            if (result.isConfirmed) {
-                Swal.fire(
-                '¡Gracias por tu compra!'
-                )
-                articulosCarrito = [];
-                localStorage.setItem("articulosCarrito", JSON.stringify(articulosCarrito));
-                limpiarHTML();
-            }
-            })
+      Swal.fire({
+        title: '¿Confirmar compra?',
+        text: "Recuerda revisar bien tus datos",
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#303030',
+        cancelButtonText: 'Seguir comprando',
+        confirmButtonText: 'Confirmar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            '¡Gracias por tu compra!'
+          )
+          articulosCarrito = [];
+          localStorage.setItem("articulosCarrito", JSON.stringify(articulosCarrito));
+          limpiarHTML();
+        }
+      })
     }
-  
-});
-}
-//FUNCIONES
 
-// eventos mouse para c/ boton agregar al carrito
+  });
+}
+
 function agregarProducto(e) {
   e.preventDefault();
 
@@ -134,18 +155,15 @@ function agregarProducto(e) {
     }).showToast();
   }
 }
-//elminar producto del carrito
 
 function eliminarProducto(e) {
   e.preventDefault();
-  // console.log(e.target.classList);
   if (e.target.classList.contains("borrar-producto")) {
     const productoID = e.target.getAttribute("id");
 
-    //Eliminar del arreglo de articulosCarrito por el id
     articulosCarrito = articulosCarrito.filter((producto) => producto.id !== productoID);
-    localStorage.setItem("articulosCarrito",JSON.stringify(articulosCarrito));
-    console.log(articulosCarrito)
+    localStorage.setItem("articulosCarrito", JSON.stringify(articulosCarrito));
+
     carritoHTML();
     Toastify({
       text: "Producto eliminado con éxito",
@@ -157,7 +175,6 @@ function eliminarProducto(e) {
   }
 }
 
-// Extraer de HTML el producto
 function leerDatosProducto(producto) {
   const infoProducto = {
     imagen: producto.querySelector("img").src,
@@ -167,46 +184,41 @@ function leerDatosProducto(producto) {
     cantidad: 1,
   };
 
-  //Revisa si un elemento ya existe en el carrito
-
   const existe = articulosCarrito.some((producto) => producto.id === infoProducto.id);
   if (existe) {
-    //Actualizamos la cantidad
     const productos = articulosCarrito.map((producto) => {
       if (producto.id === infoProducto.id) {
         producto.cantidad++;
-        return producto; // retorna objeto actualizado
+        return producto; 
       } else {
-        return producto; // retorna los objetos que no son duplicados
+        return producto;
       }
     });
     articulosCarrito = [...productos];
-    localStorage.setItem("articulosCarrito",JSON.stringify(articulosCarrito));
+    localStorage.setItem("articulosCarrito", JSON.stringify(articulosCarrito));
   } else {
-    //Agregar elementos al arreglo del carrito
     articulosCarrito = [...articulosCarrito, infoProducto];
-    localStorage.setItem("articulosCarrito",JSON.stringify(articulosCarrito));
-    
+    localStorage.setItem("articulosCarrito", JSON.stringify(articulosCarrito));
+
   }
   carritoHTML();
 }
 
-//Muestra el Carrito en el HTML
-
 function carritoHTML() {
-  
+
   limpiarHTML();
-  
-  //Recorre el carrito y genera el HTML
+
   articulosCarrito.forEach((producto) => {
     const row = document.createElement("tr");
-    row.innerHTML = `<td>${producto.titulo}</td>
-    <td>${producto.precio}</td>
-    <td>${producto.cantidad}</td>
+    row.innerHTML = `
     <td>
     <a href="" class="borrar-producto" id="${producto.id}">x</a>
-    </td>`;
-    //Agrega el HTML del carrito en el tbody
+    </td>
+    <td>${producto.titulo}</td>
+    <td>${producto.cantidad}</td>
+    <td>${producto.precio}</td>
+    `;
+
     contenedorCarrito.appendChild(row);
     total.textContent = articulosCarrito.length;
   });
@@ -215,29 +227,46 @@ function carritoHTML() {
 function limpiarHTML() {
   contenedorCarrito.innerHTML = "";
   total.textContent = articulosCarrito.length;
+  calculateTotal();
 }
 
- const buscarUnProducto = () => {
-   fetch('https://api.mercadolibre.com/sites/MLA/search?q=lentes_rusty')
-   .then((response) => response.json())
-   .then(informacion=> {
-     let acumulador = ``;
-     informacion.results.forEach((producto) => {
-       console.log(producto);
-       acumulador += `
-       <div class="card">
+const buscarUnProducto = () => {
+  fetch('https://api.mercadolibre.com/sites/MLA/search?q=lentes_rusty')
+    .then((response) => response.json())
+    .then(informacion => {
+      let acumulador = ``;
+      informacion.results.forEach((producto) => {
+        
+        acumulador += `
+      <div class="card">
       <div>
       <img class="img" src="${producto.thumbnail}" alt="">
       </div>
-      <span class="price-tag"><span>$</span>${producto.price}</span>
-      
-      <p class ="card-title">${producto.title}</p>
-      <a href="" class="btn agregar-carrito" id="${producto.id}">Comprar</a>
+      <div>
+      <span class="price-tag-symbol">$</span><span class="price-tag">${producto.price}</span>
       </div>
-       `;
-       document.getElementById('seccion-card').innerHTML = acumulador;
-     })
-   })
-   cargarEventListeners()
- };
- buscarUnProducto();
+      <span class="free-shipping">Envio gratis</span>
+      <p class ="card-title card-title-mod">${producto.title}</p>
+      
+      <a href="#" class="btn agregar-carrito" id="${producto.id}">Comprar</a>
+      </div>`;
+        document.getElementById('seccion-card').innerHTML = acumulador;
+      })
+    })
+  cargarEventListeners()
+};
+buscarUnProducto();
+
+const openModal = document.querySelector('.hero__cta');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.modal__close');
+
+let open = openModal.addEventListener('click', (e) => {
+  e.preventDefault();
+  modal.classList.add('modal--show');
+});
+
+let close = closeModal.addEventListener('click', (e) => {
+  e.preventDefault();
+  modal.classList.remove('modal--show');
+});
